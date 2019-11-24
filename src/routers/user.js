b8d15b02437e39fa08be891512c43d27afb75757
omar <omaralhoori@gmail.com>
2019-11-24 11:47:09 +0300

    add token login

--- src/routers/user.js
+++ src/routers/user.js
@@ -5,6 +5,7 @@ const auth = require('../middleware/auth')
 const router = new express.Router()
 const { sendWelcomeEmail ,sendCancelationEmail} = require('../emails/account.js')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 router.get('/users', async (req, res) => {
     User.find({}).then(users=>{
@@ -12,6 +13,22 @@ router.get('/users', async (req, res) => {
     }).catch(e=>{
         res.status(500).send()
     })
+})
+
+router.post('/users/token', async (req, res)=>{
+    try{
+        const token = req.body.token
+        
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        const user = await User.findOne({_id: decoded._id, 'tokens.token': token})
+        console.log(user)
+        if(!user){
+            throw new Error()
+        }
+        res.send(user)
+    }catch(e){
+        res.status(401).send({ error: 'Pleaset authenticate.'})
+    }
 })
 
 router.post('/users', async (req, res) => {
